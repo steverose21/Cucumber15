d4035b5cf635288a53720bd70504418b62dfffa3
Ashish Mishra <ashish.mishra.sse@gmail.com>
2023-04-12 07:30:41 +0530

    -Data Table

--- src/test/java/StepDefinitions/Login.java
+++ src/test/java/StepDefinitions/Login.java
@@ -1,5 +1,6 @@
 package StepDefinitions;
 
+import Pages.LoginPage;
 import Utils.CommonMethods;
 import Utils.ConfigReader;
 import io.cucumber.datatable.DataTable;
@@ -9,6 +10,7 @@ import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.bidi.log.Log;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -31,23 +33,15 @@ public class Login extends CommonMethods {
 
     @When("user enters valid email and valid password")
     public void user_enters_valid_email_and_valid_password() {
-
-        //driver.findElement(By.id("txtUsername")).sendKeys(ConfigReader.getPropertyValue("username"));
-
-        WebElement usernameTextBox = driver.findElement(By.id("txtUsername"));
-        sendText(usernameTextBox, ConfigReader.getPropertyValue("username"));
-
-        //driver.findElement(By.id("txtPassword")).sendKeys(ConfigReader.getPropertyValue("password"));
-        WebElement passwordTextBox = driver.findElement(By.id("txtPassword"));
-        sendText(passwordTextBox, ConfigReader.getPropertyValue("password"));
-
+        LoginPage login = new LoginPage();
+        sendText(login.usernameTextBox, ConfigReader.getPropertyValue("username"));
+        sendText(login.passwordTextBox, ConfigReader.getPropertyValue("password"));
     }
 
     @When("click on login button")
     public void click_on_login_button() {
-
-        WebElement loginBtn = driver.findElement(By.id("btnLogin"));
-        doClick(loginBtn);
+        LoginPage login = new LoginPage();
+        doClick(login.loginBtn);
 
     }
 
@@ -66,34 +60,23 @@ public class Login extends CommonMethods {
 
     @When("user enters valid {string} and valid {string}")
     public void user_enters_valid_and_valid(String username, String password) {
-        WebElement usernameTextBox = driver.findElement(By.id("txtUsername"));
-        sendText(usernameTextBox, username);
-
-        WebElement passwordTextBox = driver.findElement(By.id("txtPassword"));
-        sendText(passwordTextBox, password);
+        LoginPage login = new LoginPage();
+        sendText(login.usernameTextBox, username);
+        sendText(login.passwordTextBox, password);
     }
 
     @When("user enters username and password and verifies login")
     public void user_enters_username_and_password_and_verifies_login(DataTable dataTable) {
-
+        LoginPage login = new LoginPage();
         List<Map<String, String>> userCredentials = dataTable.asMaps();
         for (Map<String, String> userCreds : userCredentials) {
             String username = userCreds.get("username");
             String password = userCreds.get("password");
-
-            WebElement usernameTextBox = driver.findElement(By.id("txtUsername"));
-            sendText(usernameTextBox, username);
-            WebElement passwordTextBox = driver.findElement(By.id("txtPassword"));
-            sendText(passwordTextBox, password);
-
-            WebElement loginBtn = driver.findElement(By.id("btnLogin"));
-            doClick(loginBtn);
-
-            WebElement welcomeIcon= driver.findElement(By.id("welcome"));
-            doClick(welcomeIcon);
-
-            WebElement logoutLink=driver.findElement(By.xpath("//a[text()='Logout']"));
-            doClick(logoutLink);
+            sendText(login.usernameTextBox, username);
+            sendText(login.passwordTextBox, password);
+            doClick(login.loginBtn);
+            doClick(login.welcomeIcon);
+            doClick(login.logoutLink);
 
 
         }
--- src/test/java/TestRunner/SmokeRunner.java
+++ src/test/java/TestRunner/SmokeRunner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(features = "src\\test\\resources\\Features",
         glue = "StepDefinitions",
         dryRun = false,
-        tags = "@dataTable",
+        tags = "@smoke",
         plugin = {"pretty"}
 
 )
