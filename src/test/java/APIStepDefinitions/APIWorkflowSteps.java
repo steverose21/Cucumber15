4acb1d09bc72304410ad265dbdf2a58919f372bb
sohai <SyntaxTechnologies>
2023-05-25 07:15:03 +0530

    lc25052023

---
+++ src/test/java/APIStepDefinitions/APIWorkflowSteps.java
@@ -0,0 +1,27 @@
+package APIStepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class APIWorkflowSteps {
+
+    @Given("a request is prepared to create an employee")
+    public void a_request_is_prepared_to_create_an_employee() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("a POST call is made to create an employee")
+    public void a_post_call_is_made_to_create_an_employee() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the status code for creating an employee is {int}")
+    public void the_status_code_for_creating_an_employee_is(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
--- target/test-classes/Features/APIWorkflow.feature
+++ target/test-classes/Features/APIWorkflow.feature
@@ -1,5 +1,13 @@
 Feature: API workflow for HRMS
 
+  Background:
+    Given a JWT is generated
+
   @api
   Scenario: create an employee using API call
-    Given a JWT is generated
+    Given a request is prepared to create an employee
+    When a POST call is made to create an employee
+    Then the status code for creating an employee is 201
+
+
+
