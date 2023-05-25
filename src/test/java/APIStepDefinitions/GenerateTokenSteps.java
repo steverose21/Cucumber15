8e8ae27951e2d22ed9703ae8045b74f3eef4cdf7
sohai <SyntaxTechnologies>
2023-05-25 06:44:31 +0530

    lc25052023

---
+++ src/test/java/APIStepDefinitions/GenerateTokenSteps.java
@@ -0,0 +1,14 @@
+package APIStepDefinitions;
+
+import io.cucumber.java.en.Given;
+
+public class GenerateTokenSteps {
+
+    @Given("a JWT is generated")
+    public void a_jwt_is_generated() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+}
---
+++ target/test-classes/Features/APIWorkflow.feature
@@ -0,0 +1,5 @@
+Feature: API workflow for HRMS
+
+  @api
+  Scenario: create an employee using API call
+    Given a JWT is generated
